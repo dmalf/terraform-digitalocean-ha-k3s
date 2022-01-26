ef95e6b559f9d08971e559c6b9a96438f92a4a5d
dmalf <dommalfara@gmail.com>
2022-01-25 20:46:13 -0500

    Update project.tf

--- project.tf
+++ project.tf
@@ -1,5 +1,5 @@
 resource "digitalocean_project" "k3s_cluster" {
-  name        = "k3s-cluster"
+  name        = "k3s-cluster-${var.k3s_cluster_name}"
   description = "k3s Cluster"
   purpose     = "HA K3s (Kubernetes) Cluster"
   environment = "Development"
