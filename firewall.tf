ad746883427b8b65be024737dae4643a665a8c21
dmalf <dommalfara@gmail.com>
2022-04-07 15:15:44 -0400

    Update firewall.tf

--- firewall.tf
+++ firewall.tf
@@ -15,7 +15,7 @@ resource "digitalocean_firewall" "k3s_firewall" {
   inbound_rule {
     # Allow ICMP communication on all ports to defined 'tags' via VPC Network
     protocol         = "icmp"
-    source_addresses = [var.vpc_network_range]
+    source_addresses = ["0.0.0.0/0", "::/0"]
   }
 
   inbound_rule {
