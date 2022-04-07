2bab98a1b69bc1df775a69cfa5d0ffe1130f3fa7
dmalf <dommalfara@gmail.com>
2022-04-07 11:31:11 -0400

    Update ks3_server_init.sh

--- user_data/ks3_server_init.sh
+++ user_data/ks3_server_init.sh
@@ -9,6 +9,8 @@ apt-get install -yq \
 
 # Store Droplet ID in variable (utilises DO's Metadata Service - https://developers.digitalocean.com/documentation/metadata/)
 DROPLET_ID=$(curl -s http://169.254.169.254/metadata/v1/id)
+DROPLET_PUBLIC_ADDRESS=$(curl -s http://169.254.169.254/metadata/v1/interfaces/public/0/ipv4/address)
+
 
 # k3s
 curl -sfL https://get.k3s.io | INSTALL_K3S_CHANNEL=${k3s_channel} K3S_TOKEN=${k3s_token} sh -s - \
@@ -17,7 +19,9 @@ curl -sfL https://get.k3s.io | INSTALL_K3S_CHANNEL=${k3s_channel} K3S_TOKEN=${k3
     --kubelet-arg "provider-id=digitalocean://$DROPLET_ID" \
     --tls-san ${k3s_lb_ip} \
     --flannel-backend=${flannel_backend} \
-    --flannel-iface=eth1 \
+    --flannel-iface=eth0 \
+    --node-ip=$DROPLET_PUBLIC_ADDRESS \
+    --node-external-ip=$DROPLET_PUBLIC_ADDRESS \ 
     --disable local-storage \
     --disable-cloud-controller \
     --disable traefik \
