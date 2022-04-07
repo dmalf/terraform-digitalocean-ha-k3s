9050eef521ee5cb26f6590679e64e42c5a83813c
dmalf <dommalfara@gmail.com>
2022-04-07 11:30:19 -0400

    Update k3s_agent.sh

--- user_data/k3s_agent.sh
+++ user_data/k3s_agent.sh
@@ -9,9 +9,13 @@ apt-get install -yq \
 
 # Store Droplet ID in variable (utilises DO's Metadata Service - https://developers.digitalocean.com/documentation/metadata/)
 DROPLET_ID=$(curl -s http://169.254.169.254/metadata/v1/id)
+DROPLET_PUBLIC_ADDRESS=$(curl -s http://169.254.169.254/metadata/v1/interfaces/public/0/ipv4/address)
+
 
 # k3s
 curl -sfL https://get.k3s.io | INSTALL_K3S_CHANNEL=${k3s_channel} K3S_TOKEN=${k3s_token} K3S_URL=https://${k3s_lb_ip}:6443 sh -s - \
     --kubelet-arg 'cloud-provider=external'  \
     --kubelet-arg "provider-id=digitalocean://$DROPLET_ID" \
-    --flannel-iface=eth0
+    --flannel-iface=eth0 \
+    --node-ip=$DROPLET_PUBLIC_ADDRESS \
+    --node-external-ip=$DROPLET_PUBLIC_ADDRESS
